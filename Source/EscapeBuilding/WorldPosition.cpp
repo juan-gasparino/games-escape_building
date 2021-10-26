97f1564f291ad0e2af0bf3c6365b76cb61fa90f3
Juan Gasparino <juan.gasparino@gmail.com>
2021-10-26 09:26:49 +0200

    get position FVector

--- Source/EscapeBuilding/WorldPosition.cpp
+++ Source/EscapeBuilding/WorldPosition.cpp
@@ -2,6 +2,7 @@
 
 
 #include "WorldPosition.h"
+#include "GameFramework/Actor.h"
 
 // Sets default values for this component's properties
 UWorldPosition::UWorldPosition()
@@ -19,9 +20,24 @@ void UWorldPosition::BeginPlay()
 {
 	Super::BeginPlay();
 
-	UE_LOG(LogTemp, Display, TEXT("Display!!!!"));
-	UE_LOG(LogTemp, Warning, TEXT("Warning!!!!"));
-	UE_LOG(LogTemp, Error, TEXT("Error!!!!"));
+	//UE_LOG(LogTemp, Display, TEXT("Display!!!!"));
+	//UE_LOG(LogTemp, Warning, TEXT("Warning!!!!"));
+	//UE_LOG(LogTemp, Error, TEXT("Error!!!!"));
+
+	//FString Log = TEXT("Hello!");
+	//FString* PrtLog = &Log;
+
+	//Log.Len(); //Member for non pointers
+	//PrtLog->Len(); //arrow for pointers
+
+	//UE_LOG(LogTemp, Warning, TEXT("Non pointer: %s"), *Log);
+	//UE_LOG(LogTemp, Warning, TEXT("Pointer value: %s"), **PrtLog);
+
+	//FString ObjectName = GetOwner()->GetName();
+	//UE_LOG(LogTemp, Display, TEXT("Name actor: %s"), *ObjectName);
+
+	UE_LOG(LogTemp, Warning, TEXT("the %s is in position: %s"), *GetOwner()->GetName(), *GetOwner()->GetActorLocation().ToString());
+
 }
 
 
